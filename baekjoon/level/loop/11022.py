f02672994d138616608dc2cc1fd2ff63f94b2429
Gyeongyeong Kim <74523540+kky5738@users.noreply.github.com>
2022-12-19 17:59:42 +0900

    Solved 11022

---
+++ baekjoon/level/loop/11022.py
@@ -0,0 +1,15 @@
+import sys
+
+def solution():
+    input = sys.stdin.readline
+    t = int(input())
+
+    for i in range(t):
+        a, b = map(int, input().split(' '))
+        print(f'Case #{i+1}: {a} + {b} = {a+b}')
+
+def main():
+    solution()
+
+if __name__ == '__main__':
+    main()
