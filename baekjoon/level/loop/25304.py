aea696d4b9e8a98602cd69a9e2f08d5b5671ed23
Gyeongyeong Kim <74523540+kky5738@users.noreply.github.com>
2022-12-19 17:44:10 +0900

    Create 25304.py

---
+++ baekjoon/level/loop/25304.py
@@ -0,0 +1,18 @@
+import sys
+
+def solution():
+    x = int(sys.stdin.readline())
+    n = int(sys.stdin.readline())
+    results = 0
+
+    for _ in range(n):
+        a, b = map(int, sys.stdin.readline().split(' '))
+        results += a*b
+    
+    print('Yes' if results == x else 'No')
+
+def main():
+    solution()
+
+if __name__ == "__main__":
+    main()
