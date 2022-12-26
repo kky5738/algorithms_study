57546610c594fb51e98eac1d8e035c8f390806c6
Gyeongyeong Kim <74523540+kky5738@users.noreply.github.com>
2022-12-26 13:29:17 +0900

    Solved 1110

--- baekjoon/level/loop/1110.py
+++ baekjoon/level/loop/1110.py
@@ -1,24 +1,21 @@
-# 1 빼고는 다 가능
 import sys 
 
 def solution():
     n = sys.stdin.readline().rstrip()
-    if int(n) == 0: return print(1)
     s = n
     count = 0
-    if int(s) < 10: s = '0' + s[-1]
-    print(s)
     
     while True:
-        if s == n and count != 0: break
-        
-        m = str(int(s[0]) + int(s[1])) # 1
-        s = s[-1] + m[-1]
-        print(s)
+        if int(s) == int(n) and count != 0: break
+        if int(s) < 10:
+            s = s[-1] + s[-1]
+        else:
+            m = str(int(s[0]) + int(s[1]))
+            s = s[1] + m[-1]
         count += 1
-        # if count == 10: return
-        
+
     print(count)
+
 def main():
     solution()
 
