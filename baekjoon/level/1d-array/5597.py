19c6b4e45630c4fe20ffd47c9b7175211c7896a4
Gyeongyeong Kim <74523540+kky5738@users.noreply.github.com>
2022-12-27 12:02:34 +0900

    Solved 5597

---
+++ baekjoon/level/1d-array/5597.py
@@ -0,0 +1,19 @@
+import sys
+
+def solution():
+    answer = [i+1 for i in range(30)]
+    
+    for _ in range(28):
+        x = int(sys.stdin.readline().rstrip())
+        # answer.pop(answer.index(x))
+        answer.remove(x)
+
+    sorted(answer)
+    print(answer[0])
+    print(answer[1])
+
+        
+
+
+if __name__ == '__main__':
+    solution()
