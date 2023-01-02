df4479a125702cfbc8ffd83696fcd1ba7476badb
Gyeongyeong Kim <74523540+kky5738@users.noreply.github.com>
2023-01-02 14:28:38 +0900

    Solved 5622

--- baekjoon/level/string/5622.py
+++ baekjoon/level/string/5622.py
@@ -1,12 +1,32 @@
 import sys
 
 def solution():
-    # 다이얼-문자, 다이얼-시간
     inputs = sys.stdin.readline().rstrip()
     
-    time = [i+1 for i in range(2, 10, 1)]
-    print(time)
-    pass
+    time_dict = dict()
+    
+    for i in range(2, 10):
+        time_dict[i] = i+1
+    
+    alpha_dict = dict()
+    upper_case = 65
+    dial = 1
+    cnt = 0
+    
+    for num in range(upper_case, upper_case+26):
+        if cnt % 3 == 0 and cnt < 18:
+            dial += 1
+        if cnt > 18 and cnt < 25:
+            if cnt % 3 == 1: dial += 1
+
+        alpha_dict[chr(num)] = dial
+        cnt += 1
+    
+    answer = 0
+    for d in inputs:
+        answer += time_dict[alpha_dict[d]]
+    
+    print(answer)
 
 if __name__ == '__main__':
      solution()
