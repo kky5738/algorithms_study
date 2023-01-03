23436287e64eae294be80dc0e1aaadd84cde198a
Gyeongyeong Kim <74523540+kky5738@users.noreply.github.com>
2023-01-03 18:07:44 +0900

    Solved 1712

--- baekjoon/level/basic math 1/1712.py
+++ baekjoon/level/basic math 1/1712.py
@@ -1,8 +1,14 @@
 import sys
 
 def solution():
-    
-    pass
+    a, b, c = map(int, sys.stdin.readline().rstrip().split())
+
+    try:
+        break_even_point = int(a / (c - b)) + 1
+    except:
+        print(-1)
+    else:
+        print(break_even_point if break_even_point > 0 else -1)
 
 if __name__ == '__main__':
     solution()
