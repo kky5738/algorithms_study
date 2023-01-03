f3fc818b5ce31b2469a0131c36f13f086e2d7910
Gyeongyeong Kim <74523540+kky5738@users.noreply.github.com>
2023-01-03 14:53:22 +0900

    Sovled 5622
    Add the best answer I think

--- baekjoon/level/string/5622.py
+++ baekjoon/level/string/5622.py
@@ -3,37 +3,38 @@ import sys
 def solution():
     inputs = sys.stdin.readline().rstrip()
     
-    # time_dict = dict()
+    time_dict = dict()
     
-    # for i in range(2, 10):
-    #     time_dict[i] = i+1
+    for i in range(2, 10):
+        time_dict[i] = i+1
     
-    # alpha_dict = dict()
-    # upper_case = 65
-    # dial = 1
-    # cnt = 0
+    alpha_dict = dict()
+    upper_case = 65
+    dial = 1
+    cnt = 0
 
-    # for num in range(upper_case, upper_case+26):
-    #     if cnt % 3 == 0 and cnt < 18:
-    #         dial += 1
-    #     if cnt > 18 and cnt < 25:
-    #         if cnt % 3 == 1: dial += 1
+    for num in range(upper_case, upper_case+26):
+        if cnt % 3 == 0 and cnt < 18:
+            dial += 1
+        if cnt > 18 and cnt < 25:
+            if cnt % 3 == 1: dial += 1
 
-    #     alpha_dict[chr(num)] = dial
-    #     cnt += 1
+        alpha_dict[chr(num)] = dial
+        cnt += 1
     
-    # answer = 0
-    # for d in inputs:
-    #     answer += time_dict[alpha_dict[d]]
-    
-    # print(answer)
-
-    alpha = ['ABC', 'DEF', 'GHI', 'JKL', 'MNO', 'PQRS', 'TUV', 'WXYZ']
     answer = 0
-    for i in inputs:
-        for dial in alpha:
-            if i in dial:
-                answer += alpha.index(dial) + 3
+    for d in inputs:
+        answer += time_dict[alpha_dict[d]]
+    
     print(answer)
+    
+    # best
+    # alpha = ['ABC', 'DEF', 'GHI', 'JKL', 'MNO', 'PQRS', 'TUV', 'WXYZ']
+    # answer = 0
+    # for i in inputs:
+    #     for dial in alpha:
+    #         if i in dial:
+    #             answer += alpha.index(dial) + 3
+    # print(answer)
 if __name__ == '__main__':
      solution()
