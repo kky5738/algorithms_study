b7d1283e531436e9479604b5c1e9f28ce9ee5d9a
Gyeongyeong Kim <74523540+kky5738@users.noreply.github.com>
2023-01-03 16:13:59 +0900

    Sovled 2941

--- baekjoon/level/string/2941.py
+++ baekjoon/level/string/2941.py
@@ -1,7 +1,16 @@
 import sys
+import re
 
 def solution():
-    pass
+    inputs = sys.stdin.readline().rstrip()
+    answer = inputs
+    croatia = ('c=', 'c-', 'dz=', 'd-', 'lj', 'nj', 's=', 'z=')
+    
+    for c in croatia:
+        if c in inputs:
+            answer = re.sub(c, 'a', answer)
+
+    print(len(answer))
 
 if __name__ == '__main__':
     solution()
