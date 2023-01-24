2da50795141c56ef8060c917d5a4bb3fd3063393
Gyeongyeong Kim <74523540+kky5738@users.noreply.github.com>
2023-01-24 13:24:58 +0900

    Solved 11653

--- baekjoon/level/basic math 2/11653.py
+++ baekjoon/level/basic math 2/11653.py
@@ -3,32 +3,20 @@ from math import ceil
 
 def solution():
     n = int(sys.stdin.readline().rstrip())
-    num = [i for i in range(1, n+1)]
-    prime_list = []
+    devided = []
+    if n == 1: return
+    i = 2
 
-    for x in num:
-        if x == 1: continue
-
-        for i in range(2, ceil(x**0.5)+1):
-            prime = 1
-            if x % i == 0 and x != i:
-                prime = 0
-                break
-        if prime == 1:
-            prime_list.append(x)
-    
-    result = []
-    idx = 0
     while True:
-        
-        if n in prime_list:
-            result.append(int(n))
-            break
-                
-        n = n/prime_list[idx]
-        result.append(prime_list[idx])
-        idx += 1
-    print(result)
+        if n == 0 or i > n: break
+        if n % i == 0:
+            n //= i
+            devided.append(i)
+            continue
+        i += 1
+
+    for j in sorted(devided):
+        print(j)
 
 if __name__ == '__main__':
     solution()
