73509c38cb21039de02e51463e59d563bef8e811
Gyeongyeong Kim <74523540+kky5738@users.noreply.github.com>
2023-01-28 16:17:19 +0900

    Solved 1929
    it need more efficient code

--- baekjoon/level/basic math 2/1929.py
+++ baekjoon/level/basic math 2/1929.py
@@ -1,25 +1,23 @@
+# 백준 1929번 소수 구하기
 import sys
 from math import ceil
-import time
 
-# it maybe occur timeout
+# it maybe occur timeout -> 간신히 품.. eratos 이용해서 시간단축 가능할듯
 def solution():
     
     m, n = map(int, sys.stdin.readline().rstrip().split(' '))
-    start = time.time()
     prime_number = []
 
     for x in range(m, n+1):
         if x == 1: continue
         for i in range(2, ceil(x**0.5)+1):
-            if x % i == 0:
+            if x % i == 0 and x != i:
                 break
         else:
             prime_number.append(x)
     
     for j in prime_number:
         print(j)
-    print(time.time() - start)
 
 if __name__ == '__main__':
     solution()
