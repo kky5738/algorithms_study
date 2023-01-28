6b3c9fb71715b19aaa342957746bc4dad86ed70e
Gyeongyeong Kim <74523540+kky5738@users.noreply.github.com>
2023-01-28 16:14:03 +0900

    Solved 1193

--- baekjoon/level/basic math 1/1193.py
+++ baekjoon/level/basic math 1/1193.py
@@ -1,31 +1,33 @@
+# 백준 1193번 분수찾기
 import sys
-import math
 
 def solution():
     n = int(sys.stdin.readline().rstrip())
-    sum = 0
-
-    for i in range(1, n):
-        if sum < n:
-            sum += i
-            continue
-        level = i - 1
-        strat = sum - level + 1
-        print(level, i, strat)
-        for j in range(level, 0, -1):
-            # sum = 1+2+3+4 < num continue 대충 이런 식
-            pass
-
-        break
-
-        
     
-    pass
+    for i in range(int(n**0.5), n+1):
+        s = (i*i + i)/2   # n(n+1) / 2
+        if s >= n:
+            level = i        
+            break
+    start_idx = int(((level - 1) * (1+(level-1))) / 2) + 1
+
+    if level % 2 == 0:
+        x = 1
+        y = level
+        for _ in range(start_idx, n):
+            x += 1
+            y -= 1
+        answer = f'{x}/{y}'
+
+    else:
+        x = level
+        y = 1
+        for _ in range(start_idx, n):
+            x -= 1
+            y += 1
+        answer = f'{x}/{y}'
+
+    print(answer)
 
 if __name__ == '__main__':
     solution()
-
-"""
-1 = 1*1
-2 = 
-"""
