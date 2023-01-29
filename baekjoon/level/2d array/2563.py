03e84b8a09e3a0327585ae2223f3df1b7f48e570
Gyeongyeong Kim <74523540+kky5738@users.noreply.github.com>
2023-01-29 16:19:21 +0900

    Solved 2563

--- baekjoon/level/2d array/2563.py
+++ baekjoon/level/2d array/2563.py
@@ -1,34 +1,36 @@
 # 백준 2563번 색종이
-# 두 영역만 겹치는 경우는 맞지만 그 이상 겹치는 경우까지 고려해야 함
 import sys
-from itertools import combinations
 
 def main():
     solution()
 
+def make_square(square:list, x: int, y: int, l: int) -> list:
+
+    y_idx = l - y - 10
+    
+    for i in range(10):
+        for j in range(10):
+            square[y_idx+i][x+j] += 1
+
+    return square
+
 def solution():
+
     n = int(sys.stdin.readline().rstrip())
-    loc = []
+    l = 100
+    square = [[0 for _ in range(l)] for _ in range(l)]
+    
     for i in range(n):
         x, y = map(int, sys.stdin.readline().rstrip().split(' '))
-        loc.append([x, y])
-        
-    comb = list(combinations(loc, 2))
-    width = 0
-
-    for d1, d2 in comb:
-        x1, y1 = d1
-        x2, y2 = d2
-        
-        x_d = abs(x1 - x2)
-        y_d = abs(y1 - y2)
-
-        if x_d + y_d >= 10:
-            continue
-        else:
-            width += (10 - x_d) * (10 - y_d)
+        square = make_square(square, x, y, l)
     
-    print(n * 100 - width)
+    answer = 0
+
+    for i in range(l):
+        for j in range(l):
+            if square[i][j] > 0: answer += 1
+   
+    print(answer)
 
 if __name__ == '__main__':
     main()
