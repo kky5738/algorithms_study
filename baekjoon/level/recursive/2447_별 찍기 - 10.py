788ab4aee67db0f9d202a5d7bfe00d6a3b89bae8
Gyeongyeong Kim <74523540+kky5738@users.noreply.github.com>
2023-02-14 15:23:41 +0900

    Update 2447_별 찍기 - 10.py

--- baekjoon/level/recursive/2447_별 찍기 - 10.py
+++ baekjoon/level/recursive/2447_별 찍기 - 10.py
@@ -1,5 +1,6 @@
 # 백준 2447번 별 찍기 - 10
 import sys
+from math import ceil
 
 def main():
     solution()
@@ -12,19 +13,31 @@ def solution():
     # for i in range(n):
         # print(f'{i}//3 = {i//3},  {i} % 3 = {i % 3}, {i} / 3 = {i / 3}')
     
-    print_star(n_pow)
+    print_star(n)
 
 def print_star(n):
     global n_pow
     if n == 0: return
-    if n % n_pow**0.5 == 0 and n != n_pow:
+    
+    for i in range(int(n_pow**0.5)):
+        if n % 3 == 2 and i % 3 == 1:
+            print(' ', end='')
+            continue
+        
+        print('*', end='')
+    if n != 1:
         print()
-    # if n < n_pow // 3
-    print('*', end='')
     return print_star(n-1)
+
 
     
 # 00 01 02 10 11
 if __name__ == '__main__':
     main()
     
+    """
+    4~6
+    n/3
+    n < n // 3 or n > (n // 3) * 2
+    10~18
+    """
