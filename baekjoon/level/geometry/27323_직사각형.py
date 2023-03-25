0e8a815c7a7c754302239078b751581a596cd4e8
Gyeongyeong Kim <74523540+kky5738@users.noreply.github.com>
2023-03-25 16:31:32 +0900

    Solved 27323

---
+++ baekjoon/level/geometry/27323_직사각형.py
@@ -0,0 +1,12 @@
+# 백준 27323번 직사각형
+
+import sys
+
+def solution():
+    a = int(sys.stdin.readline().rstrip())
+    b = int(sys.stdin.readline().rstrip())
+    
+    print(a*b)
+
+if __name__ == "__main__":
+    solution()
