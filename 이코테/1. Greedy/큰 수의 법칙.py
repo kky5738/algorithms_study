ae1313ece46b070298dc34ea4975f9caac3e04d8
Gyeongyeong Kim <74523540+kky5738@users.noreply.github.com>
2024-01-22 17:48:52 +0900

    Update 큰 수의 법칙.py

--- 이코테/1. Greedy/큰 수의 법칙.py
+++ 이코테/1. Greedy/큰 수의 법칙.py
@@ -2,14 +2,15 @@ def solution():
     result = 0
     n, m, k = list(map(int, input().split()))
     arr = sorted(list(map(int, input().split())), reverse=True)
-    
+    print(arr)
     # print(8 % 3)
     # print(8 // 3)
     if arr[0] == arr[1]:
-        result = arr[0] * k
+        result = arr[0] * m
     elif arr[0] > arr[1]:
-        x = m % k
-        y = m // k
+        x = m % (k+1)
+        y = m // (k+1) * k
+        print(x,y)
         result += arr[0] * k * x + arr[1] * y
         
     return result
