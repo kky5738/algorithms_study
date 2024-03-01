d96a047435f7cec4b7cf2cce38170036b3a4aa9d
Gyeongyeong Kim <74523540+kky5738@users.noreply.github.com>
2024-03-01 18:12:18 +0900

    25206 너의 평점은

---
+++ baekjoon/level/심화 1/25206_너의 평점은.py
@@ -0,0 +1,25 @@
+# 백준 25206번 심화 1 너의 평점은
+import sys
+
+grade_table = {'A+': 4.5,
+         'A0': 4.0,
+         'B+': 3.5,
+         'B0': 3.0,
+         'C+': 2.5,
+         'C0': 2.0,
+         'D+': 1.5,
+         'D0': 1.0,
+         'F': 0.0}
+
+result = []
+n = 0.0
+for _ in range(20):
+    subject, point, grade = sys.stdin.readline().rstrip().split()
+    
+    if grade != 'P':
+        n += float(point)
+        result.append(float(point) * grade_table[grade])
+
+result = sum(result) / n
+
+print(f'{result:.6f}')
