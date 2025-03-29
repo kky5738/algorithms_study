0e7ef3d67b905f5321e64e93435b071096a258a0
Gyeongyeong Kim <74523540+kky5738@users.noreply.github.com>
2025-03-29 18:19:01 +0900

    add comment for 12865

--- baekjoon/12865.py
+++ baekjoon/12865.py
@@ -3,16 +3,16 @@ import sys
 
 def solution(n: int, m: int, stuff: list[list[int]]):
     dp = [[0] * (m+1) for _ in range(n+1)]
-    stuff = sorted(stuff)
         
     for i in range(1, n+1):
-        w, v = stuff[i-1]
-        for j in range(1, m+1):
-            dp[i][j] = dp[i-1][j]
-            if j >= w:
+        w, v = stuff[i-1]  # i번째 물건의 무게, 가치
+        for j in range(1, m+1):  # 배낭의 무게 1~m까지
+            if j >= w:  # 물건을 넣을 수 있는 경우
                 dp[i][j] = max(dp[i-1][j], dp[i-1][j-w] + v)
+            else:  # 물건을 넣을 수 없는 경우
+                dp[i][j] = dp[i-1][j]
 
-    print(dp[n][m])
+    print(dp[n][m])  # 최적해 출력
 
 if __name__ == "__main__":
     input = sys.stdin.readline
