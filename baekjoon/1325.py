53ced4c5ab40f47967347b0a1d4d6616a3c66a47
Gyeongyeong Kim <74523540+kky5738@users.noreply.github.com>
2025-04-07 11:50:43 +0900

    BOJ 1325: PyPy3 required

--- baekjoon/1325.py
+++ baekjoon/1325.py
@@ -1,67 +1,47 @@
-# BOJ 1325 효율적인 해킹
+# BOJ 1325 효율적인 해킹.
 import sys
 from collections import deque, defaultdict
 
-def bfs(start, graph, visited):
-    q = deque([start])
+input = sys.stdin.readline
+sys.setrecursionlimit(100_000)
+
+def bfs(start, graph, n):
+    visited = [False] * (n + 1)
     visited[start] = True
-    cnt = 0
+    q = deque([start])
+    count = 1
+
     while q:
-        v = q.popleft()
-        for node in graph[v]:
-            if not visited[node]:
-                cnt += 1
-                q.append(node)
-                visited[node] = True
-    
-    return [cnt, start]
+        node = q.popleft()
+        for next_node in graph[node]:
+            if not visited[next_node]:
+                visited[next_node] = True
+                q.append(next_node)
+                count += 1
+    return count
 
-def solution(n: int, m: int, graph: list[list[int]]): # 방향 설정이 잘못된 코드
-    answer = [[] for _ in range(n + 1)]
-    maxi = 0
-    for i in range(n):
-        if graph[i]:
-            visited = [False] * (n + 1)
-            cnt, com = bfs(i, graph, visited)
-            answer[cnt].append(com)
-            maxi = max(maxi, cnt)
-
-    print(' '.join(map(str, sorted(answer[maxi]))))
-
-def solution_from_gpt(n: int, m: int, graph: list[list[int]]): # 근데 이것도 시간 초과 발생ㅇ함... 다시 해보자
-    max_hack = 0
+def solution(n, m, graph):
     result = []
-    
-    def dfs(v):
-        visited[v] = True
-        cnt = 1
-        for node in graph[v]:
-            if not visited[node]:
-                cnt += dfs(node)
-        return cnt
-    
-    for i in range(1, n+1):
-        visited = [False] * (n+1)
-        count = dfs(i)
-        
-        if count > max_hack:
-            max_hack = count
-            result = [i]
-        elif count == max_hack:
+    max_count = 0
+    counts = [0] * (n + 1)
+
+    for i in range(1, n + 1):
+        counts[i] = bfs(i, graph, n)
+        if counts[i] > max_count:
+            max_count = counts[i]
+
+    for i in range(1, n + 1):
+        if counts[i] == max_count:
             result.append(i)
-    
-    print(" ".join(map(str, sorted(result))))
+
+    print(" ".join(map(str, result)))
 
 if __name__ == "__main__":
-    input = sys.stdin.readline
-    
-    n, m = map(int, input().rstrip().split())
-    # graph = [list() for _ in range(n+1)]
+    n, m = map(int, input().split())
     graph = defaultdict(list)
-    
+
     for _ in range(m):
-        a, b = map(int, input().rstrip().split())
-        graph[b].append(a)
-    
-    # solution(n, m, graph)
-    solution_from_gpt(n, m, graph)
+        a, b = map(int, input().split())
+        graph[b].append(a)  # b를 해킹하면 a도 해킹 가능
+
+    solution(n, m, graph)
